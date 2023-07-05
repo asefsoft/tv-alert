cd3e120fc5f5a6cc401589c5b1fa7c81276b33da
asefsoft@gmail.com <B3s5as3f>
2023-07-05 11:50:08 +0330

    first commit

---
+++ database/factories/TVShowFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TVShow;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TVShowFactory extends Factory
+{
+    protected $model = TVShow::class;
+
+    public function definition(): array {
+        return [
+
+        ];
+    }
+}
