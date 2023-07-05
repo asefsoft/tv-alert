ec913ae546fdc48c37a7fffe925a957d8f4956ab
M Asef <asefsoft@gmail.com>
2023-07-05 11:50:08 +0330

    first commit

---
+++ database/migrations/2023_07_03_145651_create_t_v_shows_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tv_shows', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 150)->index();
+            $table->string("permalink", 150)->unique();
+            $table->string("description", 1500);
+            $table->string("status", 30)->index();
+            $table->string("country", 30)->nullable();
+            $table->date("start_date")->nullable();
+            $table->date("end_date")->nullable();
+            $table->string("network", 30)->nullable();
+            $table->string("thumb_url", 255)->nullable();
+            $table->string("image_url", 250)->nullable();
+
+            $table->dateTime("next_ep_date")->index()->nullable();
+
+            // ep info: name, season, episode, air_date
+            $table->json("last_aired_ep")->default("[]")->comment("last aired episode info");
+            $table->json("next_ep")->default("[]")->comment("next episode to be aired");;
+
+            $table->json("genres")->default("[]");
+            $table->json("pictures")->default("[]");
+            $table->json("episodes")->default("[]");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tv_shows');
+    }
+};
