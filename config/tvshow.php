d20140b0c4deb3aeb01e80a9ebd00166c964415c
asefsoft@gmail.com <B3s5as3f>
2023-07-10 19:29:50 +0330

    added GetRemoteTVShowInfo class

---
+++ config/tvshow.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+    'api_url' => [
+        'search' => env('EPISODATE_SEARCH_URL'),
+        'tvshow_info' => env('EPISODATE_TVSHOW_INFO_URL'),
+    ]
+];
