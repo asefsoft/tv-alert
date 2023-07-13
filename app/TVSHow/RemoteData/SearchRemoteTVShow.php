4101c97d4d5fafb46b258ea60d1b9de16e66c0c7
M Asef <asefsoft@gmail.com>
2023-07-13 13:42:47 +0330

    added SearchRemoteTVShow class

--- app/TVSHow/RemoteData/SearchRemoteTVShow.php
+++ app/TVSHow/RemoteData/SearchRemoteTVShow.php
@@ -2,29 +2,30 @@
 
 namespace App\TVSHow\RemoteData;
 
+use App\Data\SearchTVShowData;
 use App\Data\TVShowData;
 
-class GetRemoteTVShowInfo
+class SearchRemoteTVShow
 {
 
     private string $errorMessage = '';
-    public function __construct(protected string $permalink) { }
+    public function __construct(protected string $query) { }
 
-    public function getTVShowInfo() : TVShowData | null {
+    public function getSearchData() : SearchTVShowData | null {
         // tv show info remote api url
-        $remoteUrl = sprintf("%s%s", config('tvshow.api_url.tvshow_info'), $this->permalink);
+        $remoteUrl = sprintf("%s%s", config('tvshow.api_url.search'), $this->query);
 
         // send remote request
         $request = new RemoteRequest($remoteUrl);
         if ($request->sendRequest()) {
             $result = $request->getResponse()->json();
             try {
-                // parse into tvshow
-                $TVShowData = TVShowData::from($result['tvShow']);
+                // parse into SearchTVShowData
+                $TVShowData = SearchTVShowData::from($result);
 
                 // could not parse
             } catch (\Exception $e) {
-                $this->errorMessage = "Empty or invalid result from remote: " . $e->getMessage();
+                $this->errorMessage = "Empty or invalid result from remote:" . $e->getMessage();
                 return null;
             }
             return $TVShowData;
