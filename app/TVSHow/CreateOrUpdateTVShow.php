d340f34d6918bc5355920b75adf5fa90ca4f27e1
asefsoft@gmail.com <B3s5as3f>
2023-07-14 16:25:40 +0330

    adde get most popular tvshows from remote

--- app/TVSHow/CreateOrUpdateTVShow.php
+++ app/TVSHow/CreateOrUpdateTVShow.php
@@ -12,19 +12,22 @@ class CreateOrUpdateTVShow
     protected CreateOrUpdateStatus $status;
     private $showOnDB;
 
-    public function __construct(protected array $tvShowInfo) {
-        $this->createOrUpdate();
+    public function __construct(protected array | TVShowData $tvShowInfo) {
+
+        if(is_array($this->tvShowInfo)) {
+            // parse array into tvshow data
+            try {
+                $showData = TVShowData::from($this->tvShowInfo);
+            } catch (\Exception $e) {
+                $this->status = CreateOrUpdateStatus::InvalidData;
+                return;
+            }
+        }
+        
+        $this->createOrUpdate($showData);
     }
 
-    private function createOrUpdate() {
-
-        // parse array into tvshow data
-        try {
-            $showData = TVShowData::from($this->tvShowInfo);
-        } catch (\Exception $e) {
-            $this->status = CreateOrUpdateStatus::InvalidData;
-            return;
-        }
+    private function createOrUpdate(TVShowData $showData) {
 
         // update last_check_date
         $showDataTobeSaved = array_merge($showData->toArray(), ['last_check_date' => now()]);
