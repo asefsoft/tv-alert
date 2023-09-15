941a9a2dc0b106abd6d388b567f8ac82f009d209
asefsoft@gmail.com <B3s5as3f>
2023-09-15 17:43:58 +0330

    added login avatar to guest template

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -72,6 +72,9 @@
                 @endif
 
                 <!-- Settings Dropdown -->
+                @auth()
+
+
                 <div class="ml-3 relative">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
@@ -122,6 +125,13 @@
                         </x-slot>
                     </x-dropdown>
                 </div>
+                @else
+                    <a href="{{ route('login') }}" title="Login">
+                        <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                            <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                        </div>
+                    </a>
+                @endauth
             </div>
 
             <!-- Hamburger -->
@@ -137,6 +147,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
+    @auth()
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
@@ -216,4 +227,5 @@
             </div>
         </div>
     </div>
+    @endauth
 </nav>
