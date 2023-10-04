76b7a1134405a5950b5923a19f61406216c4665f
asefsoft <asefsoft@gmail.com>
2023-10-04 12:40:29 +0330

    updaate welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Welcome
+            Welcome to Series Alert
         </h2>
     </x-slot>
 
     <div class="py-6 md:py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <livewire:TVShow-Group></livewire:TVShow-Group>
+                <livewire:TVShow-Group title="Recent shows" type="recent-shows"></livewire:TVShow-Group>
+                <livewire:TVShow-Group title="Last week shows" type="last-7-days-shows"></livewire:TVShow-Group>
             </div>
         </div>
     </div>
