c66c49eb3cd36d9104f3695ce694e023b02df3cb
asefsoft <asefsoft@gmail.com>
2023-10-10 14:32:51 +0330

    horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['series-alert'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
