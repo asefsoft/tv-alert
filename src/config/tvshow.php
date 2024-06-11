114624dcce673d656eec2805e1f68699c5d68067
asefsoft <asefsoft@gmail.com>
2024-06-11 12:55:34 +0330

    set better default configs

--- src/config/tvshow.php
+++ src/config/tvshow.php
@@ -2,9 +2,9 @@
 
 return [
     'api_url' => [
-        'search' => env('EPISODATE_SEARCH_URL'),
-        'tvshow_info' => env('EPISODATE_TVSHOW_INFO_URL'),
-        'most_popular' => env('EPISODATE_MOST_POPULAR_URL'),
+        'search' => env('EPISODATE_SEARCH_URL', "https://www.episodate.com/api/search?q="),
+        'tvshow_info' => env('EPISODATE_TVSHOW_INFO_URL', "https://www.episodate.com/api/show-details?q="),
+        'most_popular' => env('EPISODATE_MOST_POPULAR_URL', "https://www.episodate.com/api/most-popular"),
     ],
 
     'crawl_min_cache_hours' => env('CRAWL_MIN_CACHE_HOURS', 8),
