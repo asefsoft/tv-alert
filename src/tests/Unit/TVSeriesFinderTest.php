895a936f4daab3484d4fe50a262cabe4367ace81
asefsoft <asefsoft@gmail.com>
2025-09-01 16:27:12 +0330

    fix failed test

--- src/tests/Unit/TVSeriesFinderTest.php
+++ src/tests/Unit/TVSeriesFinderTest.php
@@ -10,7 +10,7 @@ class TVSeriesFinderTest extends TestCase
     public function it_finds_a_real_tv_series_by_title()
     {
         $finder = new TVShowImdbFinder();
-        $title = 'Doraemon (1979)'; // Known TV series
+        $title = 'Breaking Bad'; // Known TV series
         $result = $finder->findSeries($title);
 
         $this->assertTrue($result['found'], 'TV series should be found');
